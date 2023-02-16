1ba346ccb685a28122bf746484bcb52cd8e6e5c7
Rachana <rachanamcgowda@gmail.com>
2023-02-16 10:00:15 +0000

    Final commit

--- Hello1.java
+++ Hello1.java
@@ -2,6 +2,6 @@ public class Hello1
  { 
     public static void main(String[] args)
       {
-         System.out.println("Hello");
+         System.out.println("Hello Welcome to Devops");
       }
  }
